b79ebf7d1c1d7b7583298ea7c940bcfdda2c28af
rafalmalenta <rmalenta@wp.pl>
2019-08-22 12:50:18 +0200

    changed name from outerDiv to camera

--- src/functions/handleMove.js
+++ src/functions/handleMove.js
@@ -1,16 +1,19 @@
 import move from "./move";
 function handleMove(player){
     var innerDivGeometry = document.querySelectorAll(".terrain")[0].getBoundingClientRect();
-    var outerDivGeometry = document.querySelectorAll(".camera")[0].getBoundingClientRect();
+    var camera = document.querySelectorAll(".camera")[0];
+    var cameraGeometry = camera.getBoundingClientRect();
     
     var innerDivLeftOverflow = -(innerDivGeometry.x);    
-    var innerDivRightOveflow = innerDivGeometry.right - outerDivGeometry.width;
+    var innerDivRightOveflow = innerDivGeometry.right - cameraGeometry.width;
     var innerDivTopOverflow = -(innerDivGeometry.y);
-    var innerDivBottomOverflow = (innerDivGeometry.bottom - outerDivGeometry.height);
+    var innerDivBottomOverflow = (innerDivGeometry.bottom - cameraGeometry.height);
     if(player.movementVector.x > 0){
       if(innerDivRightOveflow > 0 ){
-
+        //moveCamera(-player.movementVector.x);
       }
+      else
+        player.move({x:player.movementVector.x, y:0});
     }
 
     console.log(innerDivLeftOverflow, " ",innerDivRightOveflow, " ",innerDivBottomOverflow, " ",innerDivTopOverflow, " ")
